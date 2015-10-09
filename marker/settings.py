1f692eecd4a73f84c179f9d1cac9daf7ab0f2b2c
Erik Allar <ezy023@gmail.com>
2015-10-09 07:35:03 -0500

    Update Logging settings

--- marker/settings.py
+++ marker/settings.py
@@ -102,37 +102,38 @@ LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
     'formatters': {
+        'verbose': {
+            'format' : "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
+            'datefmt' : "%d/%b/%Y %H:%M:%S"
+                    },
         'simple': {
-            'format': '[%(levelname)s] %(message)s',
-            },
-        'general': {
             'format': '[%(levelname)s] %(asctime)s %(pathname)s %(message)s',
-        },
+                    },
+            },
     'handlers': {
         'file': {
             'level': 'DEBUG',
             'class': 'logging.FileHandler',
-            'formatter': 'general',
-            'filename': os.path.join(BASE_DIR, 'logs/api.log')
+            'filename': os.path.join(BASE_DIR, 'logs/api.log'),
+            'formatter': 'verbose'
+                    },
             },
-        },
     'loggers': {
-        'django.request': {
+        'django': {
+            'handlers':['file'],
+            'propagate': True,
+            'level':'DEBUG',
+        },
+        'locations': {
             'handlers': ['file'],
             'level': 'DEBUG',
-            'propogate': True,
-            },
-        'test': {
-            'handlers':['file'],
+                    },
+        'marker': {
+            'handlers': ['file'],
             'level': 'DEBUG',
             },
-        'locations': {
-            'handlers':['file'],
-            'level': 'DEBUG',
-            },
-        },
+            }
     }
-}
 
 ALL_LOGGER = "all"
 
