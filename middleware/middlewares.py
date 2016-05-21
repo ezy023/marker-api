810edd3d253ab74913897c54747f05159626890a
Erik Allar <ezy023@gmail.com>
2016-05-20 19:41:04 -0500

    Change auth to use auth http headers

--- middleware/middlewares.py
+++ middleware/middlewares.py
@@ -10,28 +10,30 @@ class UserAuthMiddleware(object):
     def process_view(self, request, view_func, view_args, view_kwargs):
         if not getattr(view_func, 'noauth', False):
             try:
-                request_dict = self._merge_get_post_dicts(request.GET, request.POST)
-                access_token = request_dict.get('access_token')
-                if access_token:
-                    valid_user = self._auth_user_with_access_token(request, access_token)
-                    if not valid_user:
-                        return HttpResponseForbidden()
-
-                else:
-                    return HttpResponseForbidden()
-
+                self.authenticate_token_request(request)
             except Exception as e:
                 return HttpResponseForbidden()
 
         return None
 
 
+    def authenticate_token_request(self, request):
+        auth = self.get_authorization_header(request).split()
+
+        if not auth or auth[0].lower() != 'token':
+            return HttpResponseForbidden()
+
+        access_token = auth[1]
+
+        valid_user = self._auth_user_with_access_token(request, access_token)
+        if not valid_user:
+            return HttpResponseForbidden()
+
     def _auth_user_with_access_token(self, request, access_token):
         auth_user = authenticate(access_token=access_token)
         login(request, auth_user)
         return auth_user
 
-    def _merge_get_post_dicts(self, get_dict, post_dict):
-        get_dict_copy = get_dict.copy()
-        get_dict_copy.update(post_dict)
-        return get_dict_copy
+
+    def get_authorization_header(self, request):
+        return request.META.get('HTTP_AUTHORIZATION')
